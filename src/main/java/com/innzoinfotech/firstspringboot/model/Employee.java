ede7875eb35639090f6c487378ccabd0d772a23a
Rekha-git <rekha.senthil82@gmail.com>
2020-04-23 18:23:11 +0530

    added setters and getters in employee class

--- src/main/java/com/innzoinfotech/firstspringboot/model/Employee.java
+++ src/main/java/com/innzoinfotech/firstspringboot/model/Employee.java
@@ -3,9 +3,41 @@ package com.innzoinfotech.firstspringboot.model;
 public class Employee {
 	
 	private String empId;
-	private String name;
-	private String salary;
-	private String dept;
+	private String empName;	
+	private String empDepartment;
+	private String empDesignation;
+	private String empSalary;	
+	
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpDepartment() {
+		return empDepartment;
+	}
+	public void setEmpDepartment(String empDepartment) {
+		this.empDepartment = empDepartment;
+	}
+	public String getEmpDesignation() {
+		return empDesignation;
+	}
+	public void setEmpDesignation(String empDesignation) {
+		this.empDesignation = empDesignation;
+	}
+	public String getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(String empSalary) {
+		this.empSalary = empSalary;
+	}
 
 
 }
